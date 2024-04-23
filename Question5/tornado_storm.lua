a5c8eb4438ac17b741463b021d071bc741e7b3d3
braylayton <braylayton@users.noreply.github.com>
2024-04-22 20:29:37 -0600

    Question 5 - Created Tornado_storm spell
    Created the tornado storm spell.
    ran out of time but the spell has a longer duration then what I created. I also noticed the area is rotated as to not block the player.

---
+++ Question5/tornado_storm.lua
@@ -0,0 +1,19 @@
+local combat = Combat()
+
+combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
+combat:setArea(createCombatArea(AREA_CIRCLE3X3))
+
+function onGetFormulaValues(player, level, magicLevel)
+	local min = (level / 5) + (magicLevel * 1.2) + 7
+	local max = (level / 5) + (magicLevel * 2) + 12
+	return -min, -max
+end
+
+combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+
+function onCastSpell(creature, variant)
+	--Todo ran out of time but to make the spell more accurate to the video 
+	--provided it seems like the spell lasts for a certain duration as well as the area is rotated 180 I believe. 
+	return combat:execute(creature, variant)
+end
